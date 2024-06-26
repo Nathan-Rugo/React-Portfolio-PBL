181143ac70ad1ebe1ce484edc7387e5bd8958572
Nathan Rugo <168400141+Nathan-Rugo@users.noreply.github.com>
2024-06-26 23:00:54 +0300

    Project Guideline 2 Tasks

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import './App.css';
+import Body from './components/Body';
 
 
 const App = () => {
   return (
     <div id="app" className="App">
         <Header />
+        <Body />
     </div>
   );
 }
--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -1,1 +1,35 @@
 // Create your Body component here
+import { FaGithub, FaGoogle, FaLinkedin, FaStackOverflow } from "react-icons/fa";
+import Avatar from '../assets/Passport Photo.jpg';
+
+const Body = () => {
+    return (
+        <div id="body" className="body">
+            <div className="body-container">
+                <div className="body-profile">
+                    <img className="body-img" alt="avatar" src={Avatar} />
+                    <div className="body-content">
+                        <div className="body-headline">Nathan Githinji</div>
+                        <div className="body-text">Web developer</div>
+                        <div className="body-icons">
+                            <a href="https://www.linkedin.com/in/nathan-githinji-rugo-4188772b1/" target="_blank" rel="noreferrer" className="icon-link">
+                                <FaLinkedin />
+                            </a>
+                            <a href="https://github.com/Nathan-Rugo" target="_blank" rel="noreferrer" className="icon-link">
+                                <FaGithub />
+                            </a>
+                            <a href="https://stackoverflow.com/users/24143572/viscereal28" target="_blank" rel="noreferrer" className="icon-link">
+                                <FaStackOverflow />
+                            </a>
+                            <a href="mailto:nathan.githinji@strathmore.edu" target="_blank" rel="noreferrer" className="icon-link">
+                                <FaGoogle />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
